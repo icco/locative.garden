d9e0a6aa6498c190612951fa9c7d764c0f6fdb05
Nat Welch <nat@natwelch.com>
2022-01-04 16:44:48 +0000

    fixes

--- components/Cities.tsx
+++ components/Cities.tsx
@@ -29,51 +29,61 @@ class Cities extends Component {
     // For data:
     // https://en.wikipedia.org/wiki/List_of_cities_by_latitude
     // http://transition.fcc.gov/mb/audio/bickel/DDDMMSS-decimal.html
-    d3.json("cities.json", function (error, data) {
-      if (error) {
-        console.error(error)
+    d3.json(
+      "/cities.json",
+      function (
+        error,
+        data: {
+          lat: number
+          lon: number
+          name: string
+        }[]
+      ): void {
+        if (error) {
+          console.error(error)
+        }
+
+        console.log(data)
+
+        // Add dots
+        svg
+          .selectAll("circle")
+          .data(data)
+          .enter()
+          .append("circle")
+          .attr("cx", 0)
+          .attr("cy", function (d) {
+            return y(d.lat)
+          })
+          .attr("r", 5)
+
+        // Create labels
+        svg
+          .selectAll("text")
+          .data(data)
+          .enter()
+          .append("text")
+          .text(function (d) {
+            return d.name
+          })
+          .attr("x", 10)
+          .attr("y", function (d) {
+            return y(d.lat)
+          })
+          .attr("transform", "translate(5,5)")
+
+        svg
+          .append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+          .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", "1em")
+          .style("text-anchor", "end")
+          .text("Latitude")
       }
-
-      console.log(data)
-
-      // Add dots
-      svg
-        .selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", 0)
-        .attr("cy", function (d) {
-          return y(d.lat)
-        })
-        .attr("r", 5)
-
-      // Create labels
-      svg
-        .selectAll("text")
-        .data(data)
-        .enter()
-        .append("text")
-        .text(function (d) {
-          return d.name
-        })
-        .attr("x", 10)
-        .attr("y", function (d) {
-          return y(d.lat)
-        })
-        .attr("transform", "translate(5,5)")
-
-      svg
-        .append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "1em")
-        .style("text-anchor", "end")
-        .text("Latitude")
-    })
+    )
   }
 
   componentDidMount() {
