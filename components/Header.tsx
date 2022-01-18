6e87535f0e94b76b82c1bae19d2479819658cfad
Nat Welch <nat@natwelch.com>
2022-01-18 14:08:04 +0000

    doesn't work

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,22 @@ const Header = () => {
     <header>
       <Button
         onClick={() => setColorMode(colorMode === "light" ? "dark" : "light")}
+        sx={{
+          appearance: "none",
+          fontFamily: "inherit",
+          fontSize: 1,
+          fontWeight: "bold",
+          m: 0,
+          px: 2,
+          py: 2,
+          color: "text",
+          bg: "muted",
+          border: 0,
+          borderRadius: 2,
+          cursor: "pointer",
+        }}
       >
-        Toggle {colorMode === "light" ? "Dark" : "Light"}
+        {colorMode === "light" ? "Dark" : "Light"}
       </Button>
     </header>
   )
