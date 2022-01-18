1e27808786af057e608f2d9125df4ed2ada076f8
Nat Welch <nat@natwelch.com>
2022-01-18 02:28:46 +0000

    lint

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,12 @@
 /** @jsxImportSource theme-ui */
-import { ThemeProvider } from 'theme-ui'
-import Theme from 'components/Theme'
+import Theme from "components/Theme"
+import { ThemeProvider } from "theme-ui"
 
 // See https://theme-ui.com/guides/mdx-layout-components
-export default (props) => (
-  <ThemeProvider theme={Theme}>
-    <div {...props} />
-  </ThemeProvider>
-)
+export default function Layout(props) {
+  return (
+    <ThemeProvider theme={Theme}>
+      <div {...props} />
+    </ThemeProvider>
+  )
+}
--- components/Theme.ts
+++ components/Theme.ts
@@ -1,12 +1,12 @@
-import { toTheme } from '@theme-ui/typography'
-import usWeb from 'typography-theme-us-web-design-standards'
-import merge from 'lodash.merge'
-import { Nord } from '@icco/react-common'
+import { Nord } from "@icco/react-common"
+import { toTheme } from "@theme-ui/typography"
+import merge from "lodash.merge"
+import usWeb from "typography-theme-us-web-design-standards"
 
 const typography = toTheme(usWeb)
 
 export default merge(typography, {
-    colors: {
+  colors: {
     text: Nord.nord6,
     background: Nord.nord0,
     primary: Nord.nord8,
