95c0b59da5891d112726475f93b64959b669055b
Nat Welch <nat@natwelch.com>
2024-01-18 13:52:34 +0000

    wordcount

--- wordcount.sh
+++ wordcount.sh
@@ -1,3 +1,3 @@
 #! /bin/zsh
 
-for i in $(ls ./pages/stories/*.mdx); do echo $i; cat $i | wordcount; done | tee wordcount.txt
+for i in $(ls ./pages/stories/*.mdx); do echo $i; cat $i | pandoc --lua-filter wordcount.lua; done | tee wordcount.txt
